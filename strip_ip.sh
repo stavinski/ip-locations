cb740e50580cc28eea31ae0b9c53f91583cd604a
Mike Cromwell <stavinski_2000@yahoo.com>
2017-04-26 21:28:18 +0100

    adding ip_file argument

--- strip_ip.sh
+++ strip_ip.sh
@@ -2,24 +2,35 @@
 
 progname=`basename $0`
 log_file="$1"
+ip_file="$2"
 
-# check log file supplied is valid
-if [[ ! -r $log_file ]]; then
-	cat <<END
+usage()
+{
+cat <<END
 
 Parses ssh log file and strips off the IP address.
 
 Example:
 
-	$progname log_file 
+	$progname <log_file> <ip_file>
 
 END
 
   exit 1
+}
+
+# check log file supplied is valid
+if [[ ! -r $log_file ]]; then
+	usage
+fi
+
+# check ip file is supplied
+if [[ -z $ip_file ]]; then
+  usage
 fi
 
 # takes ip from ssh logger and strips the ip 
 for line in $(cut -d'-' -f6 "$log_file" | cut -d' ' -f2 | sort | uniq)
 do
-	echo $line | sed -E 's/\[|\]//g' >> 
+	echo $line | sed -E 's/\[|\]//g' >> $ip_file
 done
